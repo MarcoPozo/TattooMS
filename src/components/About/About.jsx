573126a8ced3ddb98a054659efc68ae6e2888600
MarcoPozo <marco10011111@gmail.com>
2025-05-29 14:15:23 -0500

    Se realizo responsive de Hero y About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,18 +3,18 @@ import tattooTeam from "../../assets/tattooTeam.jpg";
 
 export default function About() {
   return (
-    <section id="about" className="min-h-screen flex items-center justify-center bg-[var(--color-bg)] font-body px-8 py-20">
+    <section id="about" className="min-h-[90vh] flex items-center justify-center bg-[var(--color-bg)] font-body px-6 sm:px-8 md:px-10 xl:px-20 py-16 sm:py-20">
       <motion.div
-        className="flex flex-col md:flex-row items-center gap-12 max-w-6xl"
+        className="w-full max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-12"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
         viewport={{ once: true }}>
-        <img src={tattooTeam} alt="Nuestro equipo" className="w-full md:w-1/2 rounded-lg shadow-lg border border-[var(--color-border)]" />
+        <img src={tattooTeam} alt="Nuestro equipo" className="w-full max-w-md md:max-w-none md:w-1/2 rounded-lg shadow-lg border border-[var(--color-border)]" />
 
         <div className="text-left md:w-1/2">
-          <h2 className="text-3xl md:text-4xl font-titulo text-[var(--color-text)] mb-4">Sobre Nosotros</h2>
-          <p className="text-[var(--color-text)] leading-relaxed">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-titulo text-[var(--color-text)] mb-4">Sobre Nosotros</h2>
+          <p className="text-sm sm:text-base md:text-lg text-[var(--color-text)] leading-relaxed">
             En <span className="text-[var(--color-primary)] font-bold">TattooMS</span> somos un equipo apasionado de artistas del tatuaje con años de experiencia en la industria. Nos especializamos en
             crear diseños únicos y personalizados que reflejan la personalidad y estilo de cada cliente. Nuestro estudio es un espacio acogedor y profesional donde cada detalle cuenta, desde la
             higiene hasta la atención al cliente.
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,21 +3,26 @@ import heroImage from "../../assets/heroImage.png";
 
 export default function Hero() {
   return (
-    <section className="relative w-full h-screen bg-cover bg-top font-body" style={{ backgroundImage: `url(${heroImage})`, color: "var(--color-text)" }} id="hero">
+    <section className="relative w-full min-h-[90vh] h-screen bg-cover bg-top font-body" style={{ backgroundImage: `url(${heroImage})`, color: "var(--color-text)" }} id="hero">
       <div className="absolute inset-0 bg-black/60 flex items-center justify-center">
-        <div className="w-full flex flex-col items-start justify-center max-w-6xl mx-auto">
-          <motion.h1 className="text-5xl md:text-6xl mb-4" style={{ fontFamily: "var(--font-titulo)" }} initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
+        <div className="w-full px-6 sm:px-8 md:px-10 xl:px-0 flex flex-col items-start justify-center max-w-6xl mx-auto ">
+          <motion.h1
+            className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl mb-4"
+            style={{ fontFamily: "var(--font-titulo)" }}
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}>
             Tu historia en la piel
           </motion.h1>
 
-          <motion.p className="text-lg md:text-xl max-w-xl mb-6" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.9, delay: 0.2 }}>
+          <motion.p className="text-base sm:text-lg md:text-xl max-w-xl mb-6" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.9, delay: 0.2 }}>
             Expresa tu <span className="font-semibold text-white">individualidad</span> con un arte que llevarás por siempre
           </motion.p>
 
           <motion.button
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
-            className=" hover:bg-hover px-6 py-3 rounded-md font-semibold cursor-pointer transition"
+            className="hover:bg-hover px-6 py-3 rounded-md font-semibold cursor-pointer transition text-sm sm:text-base"
             style={{ backgroundColor: "var(--color-primary)" }}>
             Agenda una cita
           </motion.button>
